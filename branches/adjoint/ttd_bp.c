07f9eed5bf1697b38cb256b79b09ea545c508c8f
ashao <aeshao@gmail.com>
2014-07-11 17:10:23 -0700

    Added adjoint code

---
+++ branches/adjoint/ttd_bp.c
@@ -0,0 +1,91 @@
+/*
+ * ttd_bp.c
+ *
+ *  Created on: May 1, 2014
+ *      Author: ashao
+ */
+#include "init.h"
+#include "read.h"
+#include "alloc.h"
+#include <stdlib.h>
+#include "tracer_utilities.h"
+int adjointidx;
+int madjoint;
+double *mn_adjointinv, *adjointinv;
+double ***mn_ttd;
+double ***adjoint_init, ***adjoint_source;
+extern double ****tr;
+extern double misval;
+void allocate_adjoint( ){
+
+
+	int i, j, k;
+	// Set index in tracer array
+	madjoint = 0;
+	mn_adjoint = alloc3d(NZ,NXMEM,NYMEM);
+	adjoint_source = alloc3d(NZ,NXMEM,NYMEM);
+	mn_adjointinv = malloc(sizeof(double));
+	adjointinv = malloc(sizeof(double));
+	adjoint_init = alloc3d(NZ,NXMEM,NYMEM);
+
+}
+
+void initialize_adjoint(  ) {
+
+	int i,j,k;
+	extern char restart_filename[200];
+	extern int oceanmask[NXMEM][NYMEM];
+
+	for (i=0;i<NXMEM;i++)
+		for (j=0;j<NYMEM;j++)
+			for (k=0;k<NZ;k++)
+				if (oceanmask[i][j] && k < 2) {
+					adjoint_init[k][i][j] = 1.0;
+				} else
+					adjoint_init[k][i][j] = 0.0;
+
+
+	#ifdef RESTART
+	printf("Initializing adjoint from restart %s\n",restart_filename);
+	read_var3d( restart_filename, "mn_adjoint", 0, adjoint_init);
+	#endif
+
+	for (i=0;i<NXMEM;i++)
+		for (j=0;j<NYMEM;j++)
+			for (k=0;k<NZ;k++)
+				tr[madjoint][k][i][j] = adjoint_init[k][i][j];
+
+
+}
+
+void apply_adjoint_source ( ) {
+
+	int i, j, k;
+	if (adjointidx<NMONTHSADJOINT) {
+
+		for (i=0;i<NXMEM;i++)
+			for (j=0;j<NYMEM;j++)
+				for (k=0;k<NZ;k++) {
+					tr[madjoint][k][i][j] = adjoint_source[k][i][j];
+				}
+		adjointidx++;
+	}
+
+
+
+
+}
+
+void surface_adjoint( ) {
+
+	int i,j,k;
+	printf("Setting surface to 0\n");
+		for (i=0;i<NXMEM;i++)
+			for (j=0;j<NYMEM;j++)
+				for (k=0;k<2;k++)
+					tr[madjoint][k][i][j] = 0.0;
+	}
+
+	*adjointinv = tracer_inventory(madjoint);
+	printf("Adjoint Inventory: %e\n",&adjointinv);
+}
---
+++ branches/adjoint/ttd_bp.h
@@ -0,0 +1,13 @@
+/*
+ * ttd_bp.h
+ *
+ *  Created on: May 1, 2014
+ *      Author: ashao
+ */
+extern int mTTD;
+extern double ***mn_ttd;
+extern double *mn_ttdinv;
+extern double *ttdinv;
+void allocate_ttd( );
+void initialize_ttd( );
+void ttd_surface( );
